ec936e5b3162d7d8b7f3e6852816458a0445a1a7
Matthew Dinh <matthewhdinhhmk@gmail.com>
2026-09-09 01:16:49 -0700

    add version increment, version get and manifest switching commands to automation.py

--- tools/automation.py
+++ tools/automation.py
@@ -1,12 +1,87 @@
-from clypi import Command
-from typing import override
+from clypi import Command, Positional, arg
+from typing import override, Literal
+from os import path
+from pathlib import Path
+import json
 
-class Switch(Command):
-    target: 
+ROOT = Path(__file__).resolve().parent.parent
+MANIFEST_FOLDER = ROOT / "manifest"
+MANIFEST_EXTENSION = ROOT / "src" / "manifest.json"
+CHROME = MANIFEST_FOLDER / "chrome.manifest.json"
+FIREFOX = MANIFEST_FOLDER / "firefox.manifest.json"
+
+def getJson(filepath: Path) -> dict:
+    data = None
+    with filepath.open("r") as file:
+        data = json.load(file)
+    return data
+
+def getVersion(jsonDict: dict) -> list[int]:
+    version = jsonDict["version"].split(".")
+    for i in range(0, len(version)):
+        version[i] = int(version[i])
+    return version
+
+class Increment(Command):
+    position: Positional[str] = arg(help="(major|minor|patch)")
+
+    def incrementVersion(self, manifestPath: Path, pos: str) -> None:
+        data = getJson(manifestPath)
+        version = getVersion(data)
+        match self.position:
+            case "major":
+                version[0] += 1
+            case "minor":
+                version[1] += 1
+            case "patch":
+                version[2] += 1
+        data["version"] = version
+        with manifestPath.open("w+") as file:
+            json.dump(data, file, indent=2)
 
     @override
     async def run(self):
-        print("helper script")
+        self.incrementVersion(MANIFEST_EXTENSION, self.position)
+        for file_path in Path(MANIFEST_FOLDER).iterdir():
+            if file_path.is_file():
+                self.incrementVersion(file_path, self.position)
+
+class Get(Command):
+    @override
+    async def run(self):
+        print(getJson(MANIFEST_EXTENSION)["version"])
+
+class Build(Command):
+    target: str
+
+    @override
+    async def run(self):
+        print("tba")
+
+
+class Switch(Command):
+    browser: Positional[str] = arg(help="(chrome|firefox) The browser to set extension manifest for.")
+
+    @override
+    async def run(self):
+        match self.browser:
+            case "chrome":
+                with CHROME.open("r") as input:
+                    with MANIFEST_EXTENSION.open("w+") as output:
+                        output.write(input.read())
+            case "firefox":
+                with FIREFOX.open("r") as input:
+                    with MANIFEST_EXTENSION.open("w+") as output:
+                        output.write(input.read())
+            case _:
+                print("Please specify a valid browser! See switch --help.")
+
+
+class Version(Command):
+    subcommand: Increment | Get
+
+class Main(Command):
+    subcommand: Switch | Version 
 
 if __name__ == '__main__':
     cmd = Main.parse()
