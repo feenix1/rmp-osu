9c3809e37a49e3aba8fc2f3cf17c61d4cf3b69dc
Matthew Dinh <matthewhdinhhmk@gmail.com>
2026-09-13 14:22:44 -0700

    add decrement command

--- tools/automation.py
+++ tools/automation.py
@@ -22,34 +22,55 @@ def getVersion(jsonDict: dict) -> list[int]:
         version[i] = int(version[i])
     return version
 
+def incrementVersion(manifestPath: Path, pos: str, count=1) -> None:
+    data = getJson(manifestPath)
+    version = getVersion(data)
+
+    match pos:
+        case "major":
+            version[0] += count
+        case "minor":
+            version[1] += count
+        case "patch":
+            version[2] += count
+
+    if version[0] < 0:
+        version[0] = 0
+    if version[1] < 0:
+        version[1] = 0
+    if version[2] < 0:
+        version[2] = 0
+
+    data["version"] = ".".join(map(str, version))
+    with manifestPath.open("w+") as file:
+        json.dump(data, file, indent=2)
+
 class Increment(Command):
     position: Positional[str] = arg(help="(major|minor|patch)")
-
-    def incrementVersion(self, manifestPath: Path, pos: str) -> None:
-        data = getJson(manifestPath)
-        version = getVersion(data)
-        match self.position:
-            case "major":
-                version[0] += 1
-            case "minor":
-                version[1] += 1
-            case "patch":
-                version[2] += 1
-        data["version"] = version
-        with manifestPath.open("w+") as file:
-            json.dump(data, file, indent=2)
+    count: Positional[int] = arg(help="Amount to increment by. Default is 1.")
 
     @override
     async def run(self):
-        self.incrementVersion(MANIFEST_EXTENSION, self.position)
+        incrementVersion(MANIFEST_EXTENSION, self.position, self.count)
         for file_path in Path(MANIFEST_FOLDER).iterdir():
             if file_path.is_file():
-                self.incrementVersion(file_path, self.position)
+                incrementVersion(file_path, self.position, self.count)
+
+class Decrement(Command):
+    position: Positional[str] = arg(help="(major|minor|patch)")
+    count: Positional[int] = arg(help="Amount to decrement by. Default is 1.")
+
+    @override
+    async def run(self):
+        incrementVersion(MANIFEST_EXTENSION, self.position, -self.count)
+        for file_path in Path(MANIFEST_FOLDER).iterdir():
+            if file_path.is_file():
+                incrementVersion(file_path, self.position, -self.count)
 
 class Get(Command):
     @override
     async def run(self):
-        print(getJson(MANIFEST_EXTENSION)["version"])
+        print(f"Version in manifests is: {getJson(MANIFEST_EXTENSION)["version"]}")
 
 class Build(Command):
     target: str
@@ -76,9 +97,8 @@ class Switch(Command):
             case _:
                 print("Please specify a valid browser! See switch --help.")
 
-
 class Version(Command):
-    subcommand: Increment | Get
+    subcommand: Increment | Decrement | Get
 
 class Main(Command):
     subcommand: Switch | Version 
